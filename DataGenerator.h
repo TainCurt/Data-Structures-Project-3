5da5aafcd5e4f287f6fcc234877b0c6c426a81bb
TainCurt <kulikoskyyy@gmail.com>
2025-05-30 22:14:42 +0200

    DataGenerator nie działa ale już mi się nie chce

--- DataGenerator.h
+++ DataGenerator.h
@@ -1,20 +1,23 @@
 #pragma once
+#include <iostream>
 #include <vector>
 #include <memory>
 #include <string>
 #include <fstream>
 #include <random>
 #include <algorithm>
+#include "AVL.h"
+#include "LinearProbing.h"
+#include "SeparateChaining.h"
 
 using std::unique_ptr;
 using std::vector;
+using std::make_unique;
 
-vector<int> generate_base(int size, int min, int max)
+vector<int> generate_keys_unique(int size, int min, int max)
 {
     vector<int> base;
-    base.reserve(size);
-
-    for (int i = 0; i < size; i++)
+    for (int i = 0; i <= size; ++i)
     {
         base.push_back(i);
     }
@@ -23,12 +26,59 @@ vector<int> generate_base(int size, int min, int max)
     std::mt19937 gen(rd());
     std::shuffle(base.begin(), base.end(), gen);
 
+    return vector<int>(base.begin(), base.begin() + size);
+}
+
+vector<int> generate_random(int size, int min, int max)
+{
+    vector<int> base;
+    base.reserve(size);
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist(min, max);
+
+    for (int i = 0; i < size; i++)
+    {
+        base.push_back(dist(gen));
+    }
+
     return base;
 }
 
 template <class T>
-unique_ptr<T> create_and_fill(int size, int min, int max)
+vector<unique_ptr<T>> create_and_fill(int quantity, int size, int min, int max, int choice)
 {
-    auto values = generate_base(size, min, max)
-    
+    vector<int> keys;
+    switch (choice)
+    {
+    case 1:
+        keys = generate_random(size, min, max);
+        break;
+    case 2:
+        keys = generate_keys_unique(size, min, max);
+        break;
+    default:
+        std::cerr << "Invalid key generation choice. Must be 1 (random) or 2 (unique).\n";
+        return {};
+    }
+
+    vector<int> values = generate_random(size, min, max);
+
+    vector<unique_ptr<T>> data;
+    data.reserve(quantity);
+    for (int i = 0; i < quantity; i++)
+    {
+        data.push_back(make_unique<T>());
+    }
+
+    for (auto& instance : data)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            instance->insert(keys[i], values[i]);
+        }
+    }
+
+    return data;
 }
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "SeparateChaining.h"
 #include "LinearProbing.h"
 #include "AVL.h"
-//#include "DataGenerator.h"
+#include "DataGenerator.h"
 #include <vector>
 #include <algorithm>
 
@@ -13,41 +13,10 @@ using std::endl;
 
 int main()
 {
-    LinearProbing line(10);
-    line.insert(1,1);
-    line.insert(5,2);
-    line.insert(16,3);
-    line.insert(12,4);
-    line.insert(11,5);
-    line.insert(11,100);
-    // line.insert(51,6);
-    // line.insert(63,7);
-    // line.insert(18,8);
-    // line.insert(22,9);
-    // line.insert(29,10);
-    // line.insert(18,16);
-    // line.insert(28,9);
-    // line.insert(15,10);
-    // line.insert(38,11);
-    // line.insert(31,12);
-    // line.insert(20,11);
-    // line.insert(40,12);
-    // line.insert(60,13);
-    // line.insert(46,14);
-    // line.insert(7,15);
-    // line.print();
-    // line.remove(60, 13);
-    line.print();
+    //LinearProbing line(10);
+    vector<unique_ptr<LinearProbing>> copies = create_and_fill<LinearProbing>(10, 5, 0, 10, 2);
 
-// AVL tree(10);
-// tree.insert(1,2);
-// tree.insert(112,2);
-// tree.insert(6,2);
-// tree.insert(26,2);
-// tree.insert(16,2);
-// tree.insert(66,2);
-// tree.insert(76,2);
-// tree.insert(176,2);
-// tree.insert(2,2);
-// tree.print_all();
+    copies[2]->insert(1,1);
+    copies[2]->print();
+
 }
